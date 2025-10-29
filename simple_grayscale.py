eacb0160d89144eaa01cb48aa818426ea8e46873
burakkagann <burak.kagan@protonmail.com>
2025-10-29 23:54:45 +0100

    mac sync

--- simple_grayscale.py
+++ simple_grayscale.py
@@ -1,8 +1,25 @@
-import numpy as np
 from PIL import Image
+from noise import pnoise2
 
-array = np.zeros((200, 200), dtype=np.uint8)
-array += 255  # Add 255 to every element
+# Image settings
+width, height = 512, 512
+scale = 100.0  # Controls zoom level
 
-image = Image.fromarray(array)
-image.save('white_square.png')
+# Create image
+img = Image.new('RGB', (width, height))
+pixels = img.load()
+
+# Generate Perlin noise
+for y in range(height):
+    for x in range(width):
+        # Get Perlin noise value (-1 to 1)
+        noise_val = pnoise2(x / scale, y / scale, octaves=6)
+
+        # Map to 0-255 range
+        color = int((noise_val + 1) * 127.5)
+
+        # Create cloud-like blue tones
+        pixels[x, y] = (color, color, 255)
+
+img.save('perlin_clouds.png')
+img.show()
